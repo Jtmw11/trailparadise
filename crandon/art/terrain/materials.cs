a6a2a8a2941f4fce27cfd932bdbc56b62630b74e
Jtmw11 <Jtmw11@users.noreply.github.com>
2021-11-08 19:06:38 -0600

    YEEHAW

---
+++ crandon/art/terrain/materials.cs
@@ -0,0 +1,338 @@
+
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_RockyDirt";
+    detailMap = "levels/crandon/art/terrains/utah_texture_dirt_01_d";
+    internalName = "RockyDirt";
+    detailDistance = "150";
+    detailStrength = "0.7";
+    parallaxScale = "0";
+    diffuseSize = "100";
+    normalMap = "levels/crandon/art/terrains/utah_texture_dirt_01_n";
+    detailSize = "3";
+    annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_01";
+    normalMap = "levels/crandon/art/terrains/Concrete-03-ddn";
+    detailMap = "levels/crandon/art/terrains/Concrete-03-diff";
+    internalName = "Concrete";
+    detailStrength = "1";
+    diffuseSize = "20";
+    detailDistance = "100";
+    parallaxScale = "0";
+    detailSize = "5";
+    useSideProjection = "0";
+    annotation = "ROCK";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_forest_floor";
+    detailMap = "levels/crandon/art/terrains/Grass-01-D";
+    internalName = "Grass";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/crandon/art/terrains/Grass-01-N";
+    macroSize = "100";
+    macroStrength = "0.200000003";
+    macroDistance = "450";
+    macroMap = "levels/crandon/art/terrains/Macro_grass";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS";
+   useSideProjection = "0";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_02";
+    diffuseSize = "20";
+    detailMap = "levels/crandon/art/terrains/Rock-04-D";
+    detailSize = "50";
+    detailDistance = "300";
+    internalName = "Rock";
+    useSideProjection = "0";
+    detailStrength = "1";
+    normalMap = "levels/crandon/art/terrains/Rock-04-N";
+    parallaxScale = "0";
+    annotation = "ROCK";
+};
+
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_02";
+    normalMap = "levels/crandon/art/terrains/Rock-04-N";
+    detailMap = "levels/crandon/art/terrains/Rock-04-D";
+    detailDistance = "300";
+    internalName = "Rock2";
+    diffuseSize = "20";
+    detailSize = "12";
+    annotation = "ROCK";
+};
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_Grass-02b";
+    normalMap = "levels/crandon/art/terrains/Grass-01-N";
+    detailMap = "levels/crandon/art/terrains/Grass-01-D";
+    parallaxScale = "0";
+    internalName = "Grass2";
+    detailDistance = "100";
+    detailSize = "3";
+    detailStrength = "0.5";
+    macroSize = "60";
+    macroStrength = "0.3";
+    diffuseSize = "500";
+    macroDistance = "200";
+    macroMap = "levels/crandon/art/terrains/rockydirt-01-d";
+    annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_01";
+    normalMap = "levels/crandon/art/terrains/Concrete-02-NH";
+    detailMap = "levels/crandon/art/terrains/Concrete-02-D";
+    internalName = "Concrete2";
+    detailSize = "3";
+    parallaxScale = "0";
+    annotation = "ROCK";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_Sand-01";
+    normalMap = "levels/crandon/art/terrains/Sand-01-N";
+    detailMap = "levels/crandon/art/terrains/Sand-01-D";
+    internalName = "BeachSand";
+    detailStrength = "1";
+    annotation = "SAND";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_forest_floor";
+    normalMap = "levels/crandon/art/terrains/forest_floor_n";
+    detailMap = "levels/crandon/art/terrains/forest_floor_d";
+    internalName = "forest_floor";
+    detailSize = "3";
+    macroSize = "100";
+    macroStrength = "0.5";
+    macroMap = "levels/crandon/art/supernew/Desert/terrain/Overlay_Grass-01";
+    macroDistance = "900";
+    detailStrength = "0.7";
+    annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_02";
+    detailMap = "levels/crandon/art/terrains/rockybeach_d";
+    parallaxScale = "0";
+    internalName = "rocks_large";
+    detailSize = "10";
+    detailStrength = "0.5";
+    detailDistance = "100";
+    normalMap = "levels/crandon/art/terrains/rockybeach_n";
+    macroMap = "levels/crandon/art/terrains/Rock_05_detail";
+    macroSize = "35";
+    macroStrength = "0.5";
+    annotation = "ROCK";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/utah_texture_dirt_01_d";
+    normalMap = "levels/crandon/art/terrains/utah_texture_dirt_01_n";
+    detailMap = "levels/crandon/art/terrains/utah_texture_dirt_01_d";
+    internalName = "groundmodel_asphalt1";
+    detailSize = "3";
+    parallaxScale = "0";
+    macroStrength = "2";
+    annotation = "ROCK";
+   detailStrength = "1";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_forest_floor";
+    normalMap = "levels/crandon/art/terrains/forest_floor_n";
+    detailMap = "levels/crandon/art/terrains/forest_floor_d";
+    detailSize = "3";
+    detailStrength = "0.7";
+    macroMap = "levels/crandon/art/terrains/Macro_forest_floor";
+    macroSize = "100";
+    macroStrength = "0.5";
+    macroDistance = "900";
+    internalName = "Forest";
+    annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_RockyDirt";
+    diffuseSize = "100";
+    normalMap = "levels/crandon/art/terrains/utah_texture_dirt_01_n";
+    detailMap = "levels/crandon/art/terrains/utah_texture_dirt_01_d";
+    detailSize = "3";
+    detailStrength = "0.7";
+    detailDistance = "150";
+    internalName = "Dirt";
+    macroMap = "levels/crandon/art/terrains/rockydirt-01-d";
+    macroStrength = "0.15";
+    macroSize = "50";
+    macroDistance = "200";
+    annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "levels/crandon/art/terrains/Overlay_02";
+   diffuseSize = "20";
+   normalMap = "levels/crandon/art/terrains/Rock-05-N";
+   detailMap = "levels/crandon/art/terrains/Rock-05-D";
+   detailSize = "12";
+   detailStrength = "0.5";
+   detailDistance = "200";
+   macroSize = "200";
+   macroDistance = "500";
+   annotation = "ROCK";
+   internalName = "Wielka skala";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "levels/crandon/art/terrains/Overlay_02";
+   diffuseSize = "20";
+   normalMap = "levels/crandon/art/terrains/Rock-05-N";
+   detailMap = "levels/crandon/art/terrains/Rock-04-D";
+   detailSize = "12";
+   detailStrength = "0.5";
+   detailDistance = "200";
+   annotation = "ROCK";
+   internalName = "Wielkaskala";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_mud_1";
+    detailMap = "levels/crandon/art/terrains/mud_1_d";
+    macroSize = "10";
+    internalName = "Mud";
+    macroStrength = "0.4";
+    detailSize = "5";
+    detailStrength = "0.6";
+    normalMap = "levels/crandon/art/terrains/mud_1_n";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "levels/crandon/art/terrains/Overlay_02";
+   diffuseSize = "20";
+   normalMap = "levels/crandon/art/terrains/Rock-04-N";
+   detailMap = "levels/crandon/art/terrains/Rock-04-D";
+   detailSize = "50";
+   detailDistance = "800";
+   annotation = "ROCK";
+   internalName = "Rockmale";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_Grass-01";
+    detailMap = "levels/crandon/art/terrains/Grass-01-D";
+    internalName = "Grasspole";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/crandon/art/terrains/Grass-01-N";
+    macroSize = "100";
+    macroStrength = "0.4";
+    macroDistance = "450";
+    macroMap = "levels/crandon/art/terrains/Macro_grass";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_RockyDirt";
+    diffuseSize = "100";
+    normalMap = "levels/crandon/art/terrains/utah_texture_dirt_01_n";
+    detailMap = "levels/crandon/art/terrains/utah_texture_dirt_01_d";
+    detailSize = "3";
+    detailStrength = "0.7";
+    detailDistance = "150";
+    internalName = "Dirtpolowy";
+    macroMap = "levels/crandon/art/terrains/rockydirt-01-d";
+    macroStrength = "0.15";
+    macroSize = "50";
+    macroDistance = "200";
+    annotation = "GRASS";
+	
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/crandon/art/terrains/Overlay_mud_1";
+    detailMap = "levels/crandon/art/terrains/mud_1_d";
+    macroSize = "10";
+    internalName = "blototrawne";
+    macroStrength = "0.4";
+    detailSize = "10";
+    detailStrength = "1";
+    normalMap = "levels/crandon/art/terrains/mud_1_n";
+   diffuseSize = "400";
+   detailDistance = "50";
+   parallaxScale = "0";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "levels/crandon/art/terrains/Overlay_forest_floor";
+   normalMap = "levels/crandon/art/terrains/forest_floor_n";
+   detailMap = "levels/crandon/art/terrains/forest_floor_d";
+   detailSize = "3";
+   detailStrength = "0.699999988";
+   macroMap = "levels/crandon/art/terrains/Macro_forest_floor";
+   macroSize = "100";
+   macroStrength = "0.5";
+   macroDistance = "700";
+   annotation = "GRASS";
+   internalName = "forest_";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "levels/crandon/art/terrains/Overlay_forest_floor";
+   normalMap = "levels/crandon/art/terrains/forest_floor_n";
+   detailMap = "levels/crandon/art/terrains/forest_floor_d";
+   detailSize = "3";
+   detailStrength = "0.699999988";
+   macroMap = "levels/crandon/art/supernew/Desert/terrain/Overlay_Grass-01";
+   macroSize = "100";
+   macroStrength = "0.5";
+   macroDistance = "900";
+   internalName = "derby_dirt";
+   annotation = "GRASS";
+};
+
+new TerrainMaterial()
+{
+   diffuseMap = "levels/crandon/art/supernew/Desert/terrain/Overlay_Grass-01";
+   diffuseSize = "150";
+   normalMap = "levels/crandon/art/terrains/Grass-01-N";
+   detailMap = "levels/crandon/art/terrains/Grass-01-D";
+   detailSize = "3";
+   detailStrength = "0.699999988";
+   macroMap = "levels/crandon/art/terrains/Macro_grass";
+   macroSize = "100";
+   macroStrength = "0.200000003";
+   macroDistance = "450";
+   internalName = "GRASS";
+   annotation = "GRASS";
+};
